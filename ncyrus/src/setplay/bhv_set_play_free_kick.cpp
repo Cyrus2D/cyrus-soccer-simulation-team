25c2ba0be16af35c033ecc259b6029a94eaccb55
mroa4 <omidamini360@gmail.com>
2022-09-26 19:02:29 -0300

    Neck_TurnToBallOrScan (0)

--- ncyrus/src/setplay/bhv_set_play_free_kick.cpp
+++ ncyrus/src/setplay/bhv_set_play_free_kick.cpp
@@ -428,5 +428,5 @@ Bhv_SetPlayFreeKick::doMove( PlayerAgent * agent )
             agent->addSayMessage( new WaitRequestMessage() );
     }
 
-    agent->setNeckAction( new Neck_TurnToBallOrScan() );
+    agent->setNeckAction( new Neck_TurnToBallOrScan(0) );
 }
