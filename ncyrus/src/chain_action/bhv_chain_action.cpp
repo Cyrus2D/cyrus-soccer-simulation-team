ed925c3afdf7f1fbf4e36f5615104cd807d16c13
mroa4 <omidamini360@gmail.com>
2022-10-10 01:01:51 -0300

    add zero to Neck_TurnToGoalieOrScan

--- ncyrus/src/chain_action/bhv_chain_action.cpp
+++ ncyrus/src/chain_action/bhv_chain_action.cpp
@@ -254,7 +254,7 @@ Bhv_ChainAction::execute( PlayerAgent * agent )
 				__FILE__" (Bhv_ChainAction) shoot" );
 		if ( Body_ForceShoot().execute( agent ) )
 		{
-			agent->setNeckAction( new Neck_TurnToGoalieOrScan() );
+			agent->setNeckAction( new Neck_TurnToGoalieOrScan(0) );
 			return true;
         }else{
             return hold_ball(agent);
