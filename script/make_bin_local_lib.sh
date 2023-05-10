abe5fb77e11c81a75c367c863fa89fa96b6f9c0d
Nader Zare <nader.zare88@gmail.com>
2023-05-10 00:44:44 -0300

    Create make_bin_local_lib.sh

---
+++ script/make_bin_local_lib.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+bin_make=`pwd`
+cd ..
+mkdir bin -p
+cp team/src/* bin/ -r
+cd bin
+rm auto.sh *.o *.cpp *.h chain_action goalie move_def move_off neck roles setplay -r
+rm train*
+rm Makefile*
+rm debug*
+rm start.sh.in start-offline.sh
+rm cyrus_trainer
+#sed -i 's/use_sync_mode : on/#/g' player.conf
+mkdir lib -p
+cp ${bin_make}/../src/cyruslib/lib/librcsc* lib/
+cp ../script/start .
+cp ../script/kill .
+cd ..
+mv bin CYRUS -f
+tar -czvf CYRUS.tar.gz CYRUS
+rm CYRUS -rf
